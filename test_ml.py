f9c4766e6811eee1a435fb86ab2838d0308ee0b5
TypeOprogrammer <logancausey@hotmail.com>
2025-01-09 17:01:28 -0500

    ADDED FUNCTIONS TO TEST_ML.PY

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,59 @@
 import pytest
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
+from ml.model import train_model
+import numpy as np
+from ml.model import inference  
+from sklearn.ensemble import RandomForestClassifier
+
+
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_inference_return_type(trained_model, example_data):
     """
-    # add description for the first test
+    Test that inference returns a numpy array (which is the expected type).
     """
-    # Your code here
-    pass
+    X_train, _ = example_data
+    preds = inference(trained_model, X_train)
+    
+
+    
+    
+    assert isinstance(preds, np.ndarray), "Inference should return a numpy array."
+    assert preds.ndim == 1, "The predictions array should be 1-dimensional."
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+
     """
-    # add description for the second test
+    # Compute metrics for model performance
     """
-    # Your code here
-    pass
+def compute_model_metrics(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, zero_division=1)
+    recall = recall_score(y_true, y_pred, zero_division=1)
+    f1 = f1_score(y_true, y_pred, zero_division=1)
+    return accuracy, precision, recall, f1
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+
+def is_it_rf():
     """
-    # add description for the third test
+    Check if trained model uses random forest
     """
-    # Your code here
-    pass
+    
+    X_train = np.random.rand(75, 5)
+    y_train = np.random.randint(0, 2, 75)
+    
+    model = train_model(X_train, y_train)
+    
+    #returns false if model is not random forest
+    assert isinstance(model, RandomForestClassifier), "False"
+
+
+
+
+
+
