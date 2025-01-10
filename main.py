53089a242ef93b0c3c413f17881612f092348ed0
TypeOprogrammer <logancausey@hotmail.com>
2025-01-10 14:24:41 -0500

    add logic to main.py

--- main.py
+++ main.py
@@ -26,21 +26,27 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+project_path = os.getcwd()
+
+
+encoder_path = os.path.join(project_path, 'model', 'encoder.pkl')
+encoder = load_model(encoder_path)
+
+
+model_path = os.path.join(project_path, 'model', 'model.pkl')
+model = load_model(model_path)
+
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    return {"message": "Greetings and Salutations, welcome to the Income Prediction API!"}
+    
 
 
 # TODO: create a POST on a different path that does model inference
@@ -69,6 +75,11 @@ async def post_inference(data: Data):
         # use data as data input
         # use training = False
         # do not need to pass lb as input
+        data,
+        categorical_features=cat_features,
+        encoder=encoder,
+        lb=None,
+        training=False
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
